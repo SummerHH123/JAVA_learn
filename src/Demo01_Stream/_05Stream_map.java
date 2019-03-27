f2532a1aacdb6e4bdd8f4e911007bc45aadea81c
SummerHH1031 <326857289@qq.com>
2019-03-27 15:40:35 +0800

    Stream流学习--map

--- src/Demo01_Stream/_05Stream_map.java
+++ src/Demo01_Stream/_05Stream_map.java
@@ -1,6 +1,20 @@
 package Demo01_Stream;
 
+import java.util.stream.Stream;
+
 public class _05Stream_map {
+
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("1", "2", "3");
+
+        Stream<Integer> stream2 = stream.map((String s) -> {
+            return Integer.parseInt(s);
+        });
+
+        stream2.forEach(name-> System.out.println(name));
+
+
+    }
 
 
 }
