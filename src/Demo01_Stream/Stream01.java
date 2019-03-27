2190384df1d062f124f2301cde1169c4f7a135d8
SummerHH1031 <326857289@qq.com>
2019-03-27 14:23:27 +0800

    Stream流学习

--- src/Demo01_Stream/Stream01.java
+++ src/Demo01_Stream/Stream01.java
@@ -14,6 +14,35 @@ public class Stream01 {
         list.add("张强");
         list.add("张三丰");
 
+        //传统for循环
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("------------");
+
+        List<String> list1=new ArrayList<>();
+        for (String s : list) {
+            if (s.startsWith("张")){
+                list1.add(s);
+            }
+
+        }
+
+        List<String> list2=new ArrayList<>();
+        for (String s : list1) {
+            if (s.length()==3){
+                list2.add(s);
+            }
+        }
+
+        for (String s : list2) {
+            System.out.println(s);
+
+            
+        }
+        System.out.println("----------");
+
+
         //过滤数据,xuan
         list.stream()
                 .filter(name->name.startsWith("张"))
